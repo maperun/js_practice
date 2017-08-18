d306cf14a3ddf5258e6e0c4670935542a79ad2a4
Mark Perun <maperun@hotmail.com>
2017-08-18 18:53:44 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,118 @@
+// add 5 + 10
+var numOne = 5;
+var numTwo = 10;
+console.log(numOne + numTwo);
+// add 10 + 30
+var numSetOne = [5, 10, 15];
+var numSetTwo = [10, 20, 30];
+console.log(numSetOne[1] + numSetTwo[2]);
+
+// call up Barrier IPA
+var beerTypesAndBrands = [ ['IPA', 'Ale'], ['Barrier', 'Interboro'] ];
+console.log(beerTypesAndBrands[0][0]);
+console.log(beerTypesAndBrands[1][0]);
+
+//check if a variable is less than 100
+var n = 75;
+if (n < 100) {
+	console.log('Great! ' + n + ' is less than 100');
+} else {
+	console.log('Sorry, ' + n + ' is greater than 100');
+}
+
+//declare a function using name
+function enterName(name) {
+	return console.log(name);
+}
+enterName("Mark");
+enterName("Joe");
+enterName("Sally");
+
+//declare door/prize function
+function openDoor(doorNumber) {
+  if (doorNumber === 1) {
+    return console.log("movie tickets");
+  } else if (doorNumber === 2) {
+    return console.log("HDTV");
+  } else if (doorNumber === 3) {
+    return console.log("trip to Aruba");
+  } else {
+    return console.log("not a valid door #");
+  }
+}
+openDoor (1);
+openDoor (2);
+openDoor (3);
+openDoor (5);
+
+
+
+
+
+
+
+// alert("Hello World!");
+// console.log("whats up!");
+
+// var x = 10;
+// var y = 0;
+// console.log(x + y);
+
+// var colors = ["red", "orange", "yellow"];
+// console.log(colors[1], colors[2]);
+
+// console.log(colors[1] + " is " + colors[2]);
+
+// var user = {firstName: 'Mark', lastName: 'Perun', age: '38', interests: ["tennis", "golf", "traveling"]};
+// console.log(user.firstName, user.lastName);
+// console.log(user.interests[1]);
+
+// var kids = "two";
+// var spouse = "Celestine";
+// var city = "New York";
+// var occupation = "web developer";
+
+// console.log(occupation + " in " + city + " married to " + spouse + " with " + kids + " kids. ");
+
+// var num = 2;
+// if (num <= 10) {
+// 	alert("number is less than 10");
+// } else {
+// 	alert("number greater than 10");
+// }
+
+// function doTheMath(n1, n2, n3, n4) {
+// 	var result = n1 + n2 + n3 + n4;
+// 	return result;
+// }
+
+// console.log(doTheMath(300, 10, 200, 5));
+
+// function alertName() {
+// 	var result = "Hi Mark";
+// 	return result;
+// }
+
+// console.log(alertName());
+
+
+// //excerise p.12
+// function openDoor(doorNumber) {
+// 	if () {
+// 	  return alert("new car");
+// }
+	
+// }
+
+// console.log(openDoor(2));
+
+// var x = 99;
+
+// while (x >= 0) {
+// 	console.log(x);
+// 	x--;
+// }
+
+
+
+
